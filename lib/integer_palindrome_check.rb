85fc4e1be6b9ab03d78f13f28efd5b8feb7ee74b
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-28 00:19:47 -0700

    completed assignment

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,19 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
 def is_palindrome(number)
-  raise NotImplementedError
+  return false if number.class != Integer || number < 0
+  num_of_digits = get_num_of_digits(number)
+  has_palindrome = true
+  (1..num_of_digits / 2).each do |digit|
+    left_side_digit = (number / 10 ** (num_of_digits - digit)) % 10
+    right_side_digit = (number % 10 ** digit) / (10 ** (digit - 1))
+    has_palindrome = left_side_digit == right_side_digit
+    break if !has_palindrome
+  end
+  return has_palindrome
+end
+
+def get_num_of_digits(number)
+  x = 1
+  x += 1 while number / 10 ** x != 0
+  return x
 end
